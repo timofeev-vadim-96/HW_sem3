735851cc8b99afcccf7b5aef3215913f11bd518d
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2022-12-11 17:31:49 +0300

    saved 2nd task

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 ﻿// Программа, которая принимает на вход пятизначное число и проверяет, является ли оно полиндромом 
 // (симметричным числом)
-System.Console.WriteLine("Введите пятизначное число");
-int Numb = Convert.ToInt32(Console.ReadLine());
-if (Numb>=100000 || Numb<10000) System.Console.WriteLine("Это не пятизначное число");
-else
-{
-    int firstNumb = Numb/10000;
-    int secondNumb = Numb/1000%10;
-    int fourthNumb = Numb%100/10;
-    int fifthNumb = Numb%100%10;
-    if (firstNumb==fifthNumb || secondNumb==fourthNumb) System.Console.WriteLine("да");
-    else System.Console.WriteLine("нет");
-}
+System.Console.WriteLine("Введите координату x точки А");
+int xa = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату y точки А");
+int ya = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату z точки А");
+int za = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату x точки B");
+int xb = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату y точки B");
+int yb = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату z точки B");
+int zb = Convert.ToInt32(Console.ReadLine());
+double distance = Math.Sqrt(Math.Pow(xb-xa,2)+Math.Pow(yb-ya,2)+Math.Pow(zb-za,2));
+System.Console.WriteLine($"Расстояние между точками = {Math.Round(distance, 2)}");
